756942fd639e81f8f60bc226e575ddac7ff7d48d
nomanyaqoob <noman.mukhi@gmail.com>
2016-03-31 09:49:12 +0500

    group route

--- client/components/group/group.js
+++ client/components/group/group.js
@@ -44,13 +44,24 @@
             }
             that.panel.subgroupID = subgroupID;
             if (that.panel.subgroupID) {
+              if(that.panel.active == 'collaborator'){
                 CollaboratorService.getinitSubGroupDocument(that.groupID, that.panel.subgroupID, function(docId) {
                     $state.go('user.group.subgroup-' + (that.panel.active || 'activity'), { groupID: that.groupID, subgroupID: that.panel.subgroupID, docID: docId });
                 })
+              }
+              else {
+                    $state.go('user.group.subgroup-' + (that.panel.active || 'activity'), { groupID: that.groupID, subgroupID: that.panel.subgroupID});
+              }
+
             } else {
+              if(that.panel.active == 'collaborator'){
                 CollaboratorService.getinitGroupDocument(that.groupID, function(docId) {
                     $state.go('user.group.' + (that.panel.active || 'activity'), { groupID: that.groupID, docID: docId });
                 });
+              }
+              else {
+                    $state.go('user.group.' + (that.panel.active || 'activity'), { groupID: that.groupID});
+              }
             }
         };
 
