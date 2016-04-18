4d60c4c0c16b7cecc3df196239846982a12d63ee
Zia Khan <ziaukhan@hotmail.com>
2016-04-19 00:54:44 +0500

    added typings

--- serverless/userapi/typings/main.d.ts
+++ serverless/userapi/typings/main.d.ts
@@ -1,2 +1,3 @@
-/// <reference path="main/ambient/node/index.d.ts" />
 /// <reference path="main/ambient/aws/lambda.d.ts" />
+/// <reference path="main/ambient/mongoose/index.d.ts" />
+/// <reference path="main/ambient/node/index.d.ts" />
