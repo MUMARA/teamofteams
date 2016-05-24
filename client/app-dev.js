9526b6a1f5fe0ead4171297e667af7f0cf9baf52
Muhammad Kamran <smkamranqadri@yahoo.com>
2016-05-24 08:17:30 -0700

    uncomment pwa code

--- client/app-dev.js
+++ client/app-dev.js
@@ -1,15 +1,15 @@
 // Registering ServiceWorker...
-// if ('serviceWorker' in navigator) {
-//     console.log('Service Worker is supported');
-//     navigator.serviceWorker.register('/sw.js', {scope: '/'}).then(function (registration) {
-//         console.log(':^)', registration);
-//     }).catch(function (error) {
-//         console.log(':^(', error);
-//         // ref.child("user-errors").child('on-registration').push(error.toJSON(), function (err) {
-//         //     console.log('on-registration: ', err);
-//         // });
-//     });
-// } //if serviceWorker
+if ('serviceWorker' in navigator) {
+    console.log('Service Worker is supported');
+    navigator.serviceWorker.register('/sw.js', {scope: '/'}).then(function (registration) {
+        console.log(':^)', registration);
+    }).catch(function (error) {
+        console.log(':^(', error);
+        // ref.child("user-errors").child('on-registration').push(error.toJSON(), function (err) {
+        //     console.log('on-registration: ', err);
+        // });
+    });
+} //if serviceWorker
 
 /**
  * Created by ZiaKhan on 05/12/14.
